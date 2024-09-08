8379ee3b9917330026445c245bf5c29649a0fc6f
sibyl_babu@gmail.com <sibyl_babu@gmail.com>
2024-09-08 19:39:00 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.routes.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
 const app = express();
-app.post("/products", (req, res) => {});
 
-app.listen(5000, () => {
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
   connectDB();
   console.log("Server started at http://localhost:5000");
 });
